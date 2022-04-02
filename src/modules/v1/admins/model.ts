e58cd455d9d480b821635866e2f27ac441d497db
José Lucas <josluc2016@gmail.com>
2022-04-02 12:38:14 -0300

    feat: models created

---
+++ src/modules/v1/admins/model.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose'
+import { createHash } from 'helpers/utils'
+
+export interface AdminInterface {
+  email: string
+  password: string
+}
+
+const schema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      set: createHash,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.__v
+        return ret
+      },
+    },
+  },
+)
+
+const Model = model<AdminInterface>('admins', schema)
+
+export default Model
---
+++ src/modules/v1/workers/model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, model } from 'mongoose'
+import { createHash } from 'helpers/utils'
+
+export interface WorkersInterface {
+  email: string
+  password: string
+  managerId: mongoose.Types.ObjectId
+}
+
+const schema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      set: createHash,
+    },
+    managerId: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        delete ret.__v
+        return ret
+      },
+    },
+  },
+)
+
+const Model = model<WorkersInterface>('workers', schema)
+
+export default Model
